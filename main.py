038f43745526ed615db7440d32e6b5345b794c8b
maguroooo <maguroshouta@gmail.com>
2021-09-14 21:40:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,42 +7,41 @@ import tkinter as tk
 pyocr.tesseract.TESSERACT_CMD = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 root = tk.Tk()
-root.title("tk")
+root.title("Python OCR Test")
 root.geometry("700x500")
 
-def test():
-    txtbox1.insert(tk.END, "1234")
+engines = pyocr.get_available_tools()
+engine = engines[0]
 
-def clear_text():
-    txtbox1.delete(0, tk.END)
+
+def load():
+    typ = [('すべてのファイル', '*')]
+    dir = 'C:\\'
+    file = filedialog.askopenfilename(filetypes=typ, initialdir=dir)
+    global txt
+    txt = engine.image_to_string(Image.open(file), lang="jpn")
+    output()
+
+
+def output():
+    textbox.insert(tk.END, txt)
+
 
 def copy_text():
-    txt = txtbox1.get()
-    print(txt)
-    root.clipboard_append(txt)  # ここがクリップボードへのコピーをする処理
+    txt = textbox.get()
+    root.clipboard_append(txt)
 
-button1 = tk.Button(root, text="サンプル文字を出力", font=("Meiryo UI",18), command=test)
-button1.pack(expand=1)
 
-txtbox1 = tk.Entry(font=("Meiryo UI",18), width=30)
-txtbox1.pack(expand=1)
+loadbtn = tk.Button(root, text="画像読み込み", font=("Meiryo UI", 18), command=load)
+loadbtn.pack(expand=1)
 
-button2 = tk.Button(root, text="テキストボックス内をクリア", font=("Meiryo UI",18), command=clear_text)
-button2.pack(expand=1)
+textbox = tk.Entry(font=("Meiryo UI", 18), width=30)
+textbox.pack(expand=10)
 
-button3 = tk.Button(root, text="テキストボックス内をコピー", font=("Meiryo UI",18), command=copy_text)
-button3.pack(expand=1)
+copybtn = tk.Button(root, text="テキストをクリップボードにコピー", font=("Meiryo UI", 18), command=copy_text)
+copybtn.pack(expand=1)
 
 root.mainloop()
 
-# typ = [('すべてのファイル', '*')]
-# dir = 'C:\\'
-# file = filedialog.askopenfilename(filetypes=typ, initialdir=dir)
-#
-# # OCRエンジンを取得
-# engines = pyocr.get_available_tools()
-# engine = engines[0]
-#
-# # 画像の文字を読み込む
-# txt = engine.image_to_string(Image.open(file), lang="jpn")
-# print(txt)
+
+
